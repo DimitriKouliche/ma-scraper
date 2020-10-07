301d0615d2a46c8a125d23f6cf824f5d4bb0d9c9
Dimitri Kouliche <d.kouliche@gmail.com>
2020-10-07 23:06:18 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,12 +6,6 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv()
-
-# OR, the same with increased verbosity
-load_dotenv(verbose=True)
-
-# OR, explicitly providing path to '.env'
 from pathlib import Path  # Python 3.6+ only
 
 env_path = Path('.') / '.env'
